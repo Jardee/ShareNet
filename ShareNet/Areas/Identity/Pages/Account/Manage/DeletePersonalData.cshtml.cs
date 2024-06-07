0556a8ef6491cfe49637c9f504cf34b7d069fd3c
Dominik Balicki <dominik.balicki@outlook.com>
2024-06-07 14:14:37 +0200

    poprawki

--- ShareNet/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
+++ ShareNet/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using ShareNet.Data;
 
 namespace ShareNet.Areas.Identity.Pages.Account.Manage
 {
@@ -17,15 +18,20 @@ namespace ShareNet.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<DeletePersonalDataModel> _logger;
+        private readonly IConfiguration Configuration;
+        private readonly ApplicationDbContext _context;
 
         public DeletePersonalDataModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
-            ILogger<DeletePersonalDataModel> logger)
+            ILogger<DeletePersonalDataModel> logger,
+            IConfiguration configuration, ApplicationDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
+            Configuration = configuration;
+            _context = context;
         }
 
         /// <summary>
@@ -92,6 +98,26 @@ namespace ShareNet.Areas.Identity.Pages.Account.Manage
             {
                 throw new InvalidOperationException($"Unexpected error occurred deleting user.");
             }
+
+            //usuwanie danych użytkownika przy usunięciu konta
+
+            //zip jeżeli istnieje z danymi pobranymi w bulku
+            string zipFilePath = Configuration["AppSettings:UsersFolderForFiles"] + "/" + user.Id + ".zip";
+            bool zipExists = System.IO.File.Exists(zipFilePath); if (zipExists) System.IO.File.Delete(zipFilePath);
+
+            //folder użytkownika
+            string userFolderPath = Configuration["AppSettings:UsersFolderForFiles"] + "/" + user.Id;
+            new DirectoryInfo(userFolderPath).Delete(true);
+
+            //logi
+            var loglist = _context.Logi.Where(x => x.UserId == user.Id);
+            _context.Logi.RemoveRange(loglist);
+
+            //pliki w bazie
+            var filelist = _context.Files.Where(x => x.UserId == user.Id);
+            _context.Files.RemoveRange(filelist);
+
+            _context.SaveChanges();
 
             await _signInManager.SignOutAsync();
 
